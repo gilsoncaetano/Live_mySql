c8c8064fc9a29596bcdbb866f5b4d85ca303c6ca
Gilson Caetano <53578134+gilsoncaetano@users.noreply.github.com>
2020-04-13 19:24:04 -0300

    Atualização do banco de dados para gera os

--- dbInfox.sql
+++ dbInfox.sql
@@ -48,5 +48,33 @@ values('Linus Torvalds','Rua Tux, 2015','6060-6060','linus@linus.com','07175-400
 
 select * from tbclientes;
 
+reate table tbos(
+os int primary key auto_increment,
+data_os timestamp default current_timestamp,
+equipamento varchar(150) not null,
+defeito varchar(150) not null,
+servico varchar(150),
+tecnico varchar(30),
+valor decimal(10,2),
+idcli int not null,
+foreign key(idcli) references tbclientes(idcli)
+);
+
+describe tbos;
+
+insert into tbos (equipamento,defeito,servico,tecnico,valor,idcli)
+values ('Notebook','Não liga','Troca da fonte','Caetano',87.70,1);
+
+select * from tbos;
+
+-- o código abaixo traz informações de duas tabelas
+select
+O.os,equipamento,defeito,servico,valor,
+C.nomecli,fonecli
+from tbos as O
+inner join tbclientes as C
+on (O.idcli = C.idcli);
+   
+
 
 
