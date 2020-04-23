f619f0ffb0fafdedb7d0e627de8bdabdc9c929d8
Gilson Caetano <53578134+gilsoncaetano@users.noreply.github.com>
2020-04-23 01:22:13 -0300

    Atividade 01 do Infox

--- Aula_03/Infox.sql
+++ Aula_03/Infox.sql
@@ -6,10 +6,15 @@
 
 -- Comentários
 -- a linha abaixo cria um bancode dados.alter
+
 create database dbinfox;
+
 -- a linha abaixo escolhe o banco de dados
+
 use dbinfox;
+
 -- o bloco de instruções abaixo cria uma tabela
+
 create table tbusuarios(
 iduser int primary key,
 usuario varchar(50) not null,
@@ -19,13 +24,18 @@ senha varchar(15) not null
 );
 
 -- o comando abaixo descreve a tabela
+
 describe tbusuarios;
+
 -- a linha a baixo insere dados na tabela (CRUD)
 -- create -> insert
+
 insert into tbusuarios(iduser,usuario,fone,login,senha)
 values(1,'Gilson Caetano','2020-2020','gilsoncaetano','123456');
+
 -- a linha abaixo exibe os dados da tabela (CRUD)
 -- read -> select
+
 select * from tbusuarios;
 
 insert into tbusuarios(iduser,usuario,fone,login,senha)
@@ -34,10 +44,13 @@ insert into tbusuarios(iduser,usuario,fone,login,senha)
 values(3,'Bill Gates','2020-2020','billgates','123456');
 select*from tbusuarios;
 -- a linha abaixo modifica dados na tabela (CRUD)
--- update -> update
+-- update -> 
+
 update tbusuarios set fone='3030-3030' where iduser=2;
+
 -- a linha abaixo apaga um registro da tabela (CRUD)
 -- delete -> delete
+
 delete from tbusuarios where iduser=3;
 select * from tbusuarios;
 create table tbclientes(
@@ -48,7 +61,9 @@ fonecli varchar(50) not null,
 emailcli varchar(50),
 cepcli varchar(15)
 );
+
 describe tbclientes;
+
 insert into tbclientes(nomecli,endcli,fonecli,emailcli,cepcli)
 values('Linus Torvalds','Rua Tux, 2015','6060-6060','linus@linus.com','07175-400');
 
@@ -74,6 +89,7 @@ values ('Notebook','Não liga','Troca da fonte','Caetano',87.70,1);
 select * from tbos;
 
 -- o código abaixo traz informações de duas tabelas
+
 select
 O.os,equipamento,defeito,servico,valor,
 C.nomecli,fonecli
